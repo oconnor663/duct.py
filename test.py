1724c4b8a09518aa8141d86257c8a7798f1a5730
Jack O'Connor <oconnor663@gmail.com>
2015-08-19 22:24:04 -0400

    run()/read()/result() interface

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python
 
-from tubes import Cmd
+from tubes import Cmd, CheckedError
 
 Cmd('echo', 'hello', 'world').run()
+
+print('output: "{}"'.format(Cmd('echo', 'some     stuff').read()))
+
+print('result:', Cmd('true').result(capture_stderr=True))
+
+try:
+    Cmd('false').run()
+except CheckedError as e:
+    print('error:', e)
--- tubes.py
+++ tubes.py
@@ -1,10 +1,45 @@
+import collections
 import subprocess
 
 
 class Cmd:
     def __init__(self, prog, *args):
-        self.prog = prog
-        self.args = args
+        self._cmd = (prog,) + args
 
-    def run(self):
-        subprocess.call((self.prog,) + self.args)
+    def _run(self, check, strip=True, bytes=False, **kwargs):
+        p = subprocess.Popen(
+            self._cmd,
+            universal_newlines=not bytes,
+            **kwargs)
+        stdout, stderr = p.communicate()
+        if strip:
+            stdout = stdout and stdout.strip()
+            stderr = stderr and stderr.strip()
+        result = Result(self._cmd, p.returncode, stdout, stderr)
+        if check and p.returncode != 0:
+            raise CheckedError(result)
+        return result
+
+    def run(self, check=True):
+        return self._run(check)
+
+    def read(self, **kwargs):
+        return self.result(**kwargs).stdout
+
+    def result(self, check=True, capture_stderr=False, bytes=False,
+               strip=True):
+        stderr = subprocess.PIPE if capture_stderr else None
+        return self._run(check, stdout=subprocess.PIPE, stderr=stderr)
+
+
+Result = collections.namedtuple(
+    'Result', ['args', 'returncode', 'stdout', 'stderr'])
+
+
+class CheckedError(Exception):
+    def __init__(self, result):
+        self.result = result
+
+    def __str__(self):
+        return 'Command "{}" returned non-zero exit status {}'.format(
+            ' '.join(self.result.args), self.result.returncode)
