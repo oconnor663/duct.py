1deb40830e50fafb5e18c6ab38176d8a93b300f4
Jack O'Connor <oconnor663@gmail.com>
2015-08-19 23:16:36 -0400

    random bytes example

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ print('output: "{}"'.format(Cmd('echo', 'some     stuff').read()))
 
 print('result:', Cmd('true').result(stderr=True))
 
+print('random:', Cmd('head', '-c', 10, '/dev/urandom').read(bytes=True))
+
 try:
     Cmd('false').run()
 except CheckedError as e:
