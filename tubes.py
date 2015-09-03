d7b8f9635e841d6697c3f71028f6ead253c50d47
Jack O'Connor <oconnor663@gmail.com>
2015-09-03 00:42:48 -0400

    use threading instead of trollius/asyncio
    It just ends up being simpler. Hooking asyncio up to os pipes is a
    nightmare, and we were worried about conflicting with other asyncio
    callers. With this approach we only depend on the 'threading' and
    'subprocess' modules.

--- tubes.py
+++ tubes.py
@@ -1,8 +1,8 @@
-import atexit
 import collections
+import contextlib
 import os
-import trollius
-from trollius import From, Return
+import subprocess
+import threading
 
 
 def cmd(*args, **kwargs):
@@ -10,86 +10,54 @@ def cmd(*args, **kwargs):
 
 
 def _run_and_get_result(command, capture_stdout, capture_stderr, trim_mode,
-                        bytes_mode, check_errors):
-    # It's very unclear to me how to use the event loop properly for this. I'm
-    # doing my best to refactor these nasty details into a separate function.
-    def get_task(loop):
-        return _run_with_pipes(loop, command, capture_stdout, capture_stderr)
-    status, stdout_bytes, stderr_bytes = _run_async_task(get_task)
+                        binary_mode, check_errors):
+    status, stdout, stderr = _run_with_pipes(
+        command, capture_stdout, capture_stderr, binary_mode)
     if trim_mode:
-        stdout_bytes = _trim_bytes_or_none(stdout_bytes)
-        stderr_bytes = _trim_bytes_or_none(stderr_bytes)
-    if not bytes_mode:
-        stdout_bytes = _decode_bytes_or_none(stdout_bytes)
-        stderr_bytes = _decode_bytes_or_none(stderr_bytes)
-    result = Result(status, stdout_bytes, stderr_bytes)
+        stdout = _trim_or_none(stdout)
+        stderr = _trim_or_none(stderr)
+    result = Result(status, stdout, stderr)
     if check_errors and status != 0:
         raise CheckedError(result, command)
     return result
 
 
-# It's completely unclear to me how to run our coroutines without disrupting
-# asyncio callers outside this library. This funciton is the best I've got so
-# far. Further questions:
-#   https://redd.it/3id0fb
-#   https://gist.github.com/oconnor663/f0ddad2c0bd1f7cf14c2
-#   http://stackoverflow.com/q/32345735/823869
-def _run_async_task(get_task):
-    loop = trollius.get_event_loop()
-    atexit.register(loop.close)
-    task = get_task(loop)
-    result = loop.run_until_complete(task)
-    return result
-
-
-@trollius.coroutine
-def _run_with_pipes(loop, command, capture_stdout, capture_stderr):
-    # The subprocess module acceps None for stdin/stdout/stderr, to mean leave
-    # the default. We use that instead of hardcoding 0/1/2.
-    stdout_write = None
-    stderr_write = None
-    # Create pipes if needed, and kick off reader coroutines.
+def _run_with_pipes(command, capture_stdout, capture_stderr, binary_mode):
+    # The subprocess module acceps None for stdin/stdout/stderr, to mean "leave
+    # the default". We use that instead of hardcoding 0/1/2.
+    stdout_write = context_manager_giving_none()
+    stderr_write = context_manager_giving_none()
+    # Create pipes if needed, and kick off reader threads. Note that if we ran
+    # the child process without reader threads, it could fill up its pipe
+    # buffers and hang.
     if capture_stdout:
-        stdout_read, stdout_write, stdout_future = \
-            yield From(_create_async_pipe(loop))
+        stdout_read, stdout_write = _open_pipe(binary_mode)
+        stdout_thread = ThreadWithReturn(stdout_read.read)
+        stdout_thread.start()
     if capture_stderr:
-        stderr_read, stderr_write, stderr_future = \
-            yield From(_create_async_pipe(loop))
+        stderr_read, stderr_write = _open_pipe(binary_mode)
+        stderr_thread = ThreadWithReturn(stderr_read.read)
+        stderr_thread.start()
     # Kick off the child processes.
-    status = yield From(command._exec(loop, None, stdout_write, stderr_write))
-    stdout_bytes = None
-    stderr_bytes = None
-    if capture_stdout:
-        stdout_write.close()
-        stdout_bytes = yield From(stdout_future)
-        stdout_read.close()
-    if capture_stderr:
-        stderr_write.close()
-        stderr_bytes = yield From(stderr_future)
-        stderr_read.close()
-    raise Return(status, stdout_bytes, stderr_bytes)
-
-
-@trollius.coroutine
-def _create_async_pipe(loop):
-    # Create the pipe.
-    read_fd, write_fd = os.pipe()
-    read_pipe = os.fdopen(read_fd, 'rb')
-    write_pipe = os.fdopen(write_fd, 'wb')
-    # Hook it up to the event loop.
-    stream_reader = trollius.StreamReader()
-    yield From(loop.connect_read_pipe(
-        lambda: trollius.StreamReaderProtocol(stream_reader),
-        read_pipe))
-    # Kick off a reader. If we gave this pipe to a child process without a
-    # running reader, the child could fill up the pipe buffer and hang.
-    read_future = loop.create_task(stream_reader.read())
-    return read_pipe, write_pipe, read_future
+    with stdout_write as stdout, stderr_write as stderr:
+        status = command._exec(None, stdout, stderr)
+        stdout_bytes = None
+        stderr_bytes = None
+        if capture_stdout:
+            # This close has to happen before join() or else the reader threads
+            # will never finish. It should be safe even though the with block
+            # is also going to close it.
+            stdout_write.close()
+            stdout_bytes = stdout_thread.join()
+        if capture_stderr:
+            # Same as above.
+            stderr_write.close()
+            stderr_bytes = stderr_thread.join()
+        return Result(status, stdout_bytes, stderr_bytes)
 
 
 class CommandBase:
-    @trollius.coroutine
-    def _exec(self, loop, stdin, stdout, stderr):
+    def _exec(self, stdin, stdout, stderr):
         raise NotImplementedError
 
     def result(self, check=True, trim=False, bytes=False, stdout=True,
@@ -98,7 +66,7 @@ class CommandBase:
         # give them into more descriptive names internally.
         return _run_and_get_result(
             self, capture_stdout=stdout, capture_stderr=stderr, trim_mode=trim,
-            bytes_mode=bytes, check_errors=check)
+            binary_mode=bytes, check_errors=check)
 
     def run(self, stdout=False, **kwargs):
         return self.result(stdout=stdout, **kwargs)
@@ -133,13 +101,10 @@ class Command(CommandBase):
         else:
             self._tuple = (prog,) + args
 
-    @trollius.coroutine
-    def _exec(self, loop, stdin, stdout, stderr):
-        p = yield From(trollius.subprocess.create_subprocess_exec(
-            *self._tuple, loop=loop, stdin=stdin, stdout=stdout,
-            stderr=stderr))
-        status = yield From(p.wait())
-        raise Return(status)
+    def _exec(self, stdin, stdout, stderr):
+        status = subprocess.call(
+            self._tuple, stdin=stdin, stdout=stdout, stderr=stderr)
+        return status
 
     def __repr__(self):
         return ' '.join(self._tuple)
@@ -152,40 +117,44 @@ class OperationBase(CommandBase):
 
 
 class Then(OperationBase):
-    @trollius.coroutine
-    def _exec(self, loop, stdin, stdout, stderr):
+    def _exec(self, stdin, stdout, stderr):
         # Execute the first command.
-        status = yield From(self._left._exec(loop, stdin, stdout, stderr))
+        status = self._left._exec(stdin, stdout, stderr)
         # If it returns non-zero short-circuit.
         if status != 0:
-            raise Return(status)
+            return status
         # Otherwise execute the second command.
-        status = yield From(self._right._exec(loop, stdin, stdout, stderr))
-        raise Return(status)
+        status = self._right._exec(stdin, stdout, stderr)
+        return status
 
     def __repr__(self):
         return repr(self._left) + ' && ' + repr(self._right)
 
 
 class OrThen(OperationBase):
-    @trollius.coroutine
-    def _exec(self, loop, stdin, stdout, stderr):
+    def _exec(self, stdin, stdout, stderr):
         # Execute the first command.
-        status = yield From(self._left._exec(loop, stdin, stdout, stderr))
+        status = self._left._exec(stdin, stdout, stderr)
         # If it returns zero short-circuit.
         if status == 0:
-            raise Return(status)
+            return status
         # Otherwise ignore the error and execute the second command.
-        status = yield From(self._right._exec(loop, stdin, stdout, stderr))
-        raise Return(status)
+        status = self._right._exec(stdin, stdout, stderr)
+        return status
 
     def __repr__(self):
         return repr(self._left) + ' || ' + repr(self._right)
 
 
+# Pipe uses another thread to run the left side of the pipe in parallel with
+# the right. This is required because both the left and the right might be
+# compound expressions, where a second command might need to be started after
+# the first finishes, so someone has to be waiting on both sides at the same
+# time. There are Unix-specific ways to wait on multiple processes at once, but
+# those can conflict with other listeners that might by in the same process,
+# and they won't work on Windows anyway.
 class Pipe(OperationBase):
-    @trollius.coroutine
-    def _exec(self, loop, stdin, stdout, stderr):
+    def _exec(self, stdin, stdout, stderr):
         # Open a read/write pipe. The write end gets passed to the left as
         # stdout, and the read end gets passed to the right as stdin. Either
         # side could be a compound expression (like A.then(B)), so we have to
@@ -193,17 +162,23 @@ class Pipe(OperationBase):
         # its end of the pipe. Closing the write end allows the right side to
         # receive EOF, and closing the read end allows the left side to receive
         # SIGPIPE.
-        read_pipe, write_pipe = os.pipe()
-        lfuture = loop.create_task(self._left._exec(
-            loop, stdin, write_pipe, stderr))
-        lfuture.add_done_callback(lambda f: os.close(write_pipe))
-        rfuture = loop.create_task(self._right._exec(
-            loop, read_pipe, stdout, stderr))
-        rfuture.add_done_callback(lambda f: os.close(read_pipe))
-        lstatus = yield From(lfuture)
-        rstatus = yield From(rfuture)
-        # Return the rightmost error, or zero if no errors.
-        raise Return(lstatus if rstatus == 0 else rstatus)
+        read_pipe, write_pipe = _open_pipe(binary_mode=True)
+
+        def do_left():
+            with write_pipe:
+                return self._left._exec(stdin, write_pipe, stderr)
+        left_thread = ThreadWithReturn(target=do_left)
+        left_thread.start()
+
+        with read_pipe:
+            right_status = self._right._exec(read_pipe, stdout, stderr)
+        left_status = left_thread.join()
+
+        # Return the rightmost error, if any.
+        if right_status != 0:
+            return right_status
+        else:
+            return left_status
 
     def __repr__(self):
         return repr(self._left) + ' | ' + repr(self._right)
@@ -222,10 +197,45 @@ class CheckedError(Exception):
             self.command, self.result.returncode)
 
 
-def _trim_bytes_or_none(b):
-    newlines = b'\n\r'
-    return None if b is None else b.rstrip(newlines)
+def _trim_or_none(x):
+    newlines = b'\n\r' if isinstance(x, bytes) else '\n\r'
+    return None if x is None else x.rstrip(newlines)
 
 
 def _decode_bytes_or_none(b):
     return None if b is None else b.decode()
+
+
+def _open_pipe(binary_mode):
+    read_fd, write_fd = os.pipe()
+    read_mode, write_mode = ('rb', 'wb') if binary_mode else ('r', 'w')
+    return os.fdopen(read_fd, read_mode), os.fdopen(write_fd, write_mode)
+
+
+@contextlib.contextmanager
+def context_manager_giving_none():
+    yield None
+
+
+class ThreadWithReturn(threading.Thread):
+    def __init__(self, target, args=(), kwargs={}, **thread_kwargs):
+        super().__init__(**thread_kwargs)
+        self._target = target
+        self._args = args
+        self._kwargs = kwargs
+        self._return = None
+        self._exception = None
+
+    def run(self):
+        try:
+            self._return = self._target(*self._args, **self._kwargs)
+        except Exception as e:
+            self._exception = e
+
+    def join(self):
+        super().join()
+        if self._exception is not None:
+            # TODO: Make this python2-compatible.
+            raise RuntimeError("exception in thread", self._exception) \
+                from self._exception
+        return self._return
