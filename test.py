185e167085c94f2948d0473cbeb9ae62a0161f56
Jack O'Connor <oconnor663@gmail.com>
2015-09-04 00:55:05 -0400

    make the tests easier to read

--- test.py
+++ test.py
@@ -7,35 +7,38 @@ from duct import cmd, cd, setenv, CheckedError
 
 cmd('echo hello world').run()
 
-print('output: "{}"'.format(cmd('echo', 'some     stuff').read()))
+out = cmd('echo', 'some     stuff').read()
+print('output: "{}"'.format(out))
 
-print('result:', cmd('echo', 'more stuff').result(stderr=True))
+out = cmd('echo', 'more stuff').result(stderr=True)
+print('result:', out)
 
-print('random:', cmd('head', '-c', '10', '/dev/urandom').read(bytes=True))
+out = cmd('head', '-c', '10', '/dev/urandom').read(bytes=True)
+print('random:', out)
 
 try:
     cmd('bash', '-c', 'exit 42').run()
 except CheckedError as e:
     print('error:', e)
 
-print('pipe:',
-      cmd('cat', '/dev/zero')
-      .pipe('head', '-c', '10')
-      .pipe('cat', '-vet')
-      .read(check=False))
+out = (cmd('cat', '/dev/zero')
+       .pipe('head', '-c', '10')
+       .pipe('cat', '-vet')
+       .read(check=False))
+print('pipe:', out)
 
-print('and/or:',
-      cmd('echo', '-n', 'hi')
-      .then('false')
-      .pipe('sed', 's/hi/hee/')
-      .orthen('echo', 'haw')
-      .read())
+out = (cmd('echo', '-n', 'hi')
+       .then('false')
+       .pipe('sed', 's/hi/hee/')
+       .orthen('echo', 'haw')
+       .read())
+print('and/or:', out)
 
-print('nesting:',
-      cmd('echo moomoo')
-      .pipe(cmd('head -c 3').pipe('sed s/o/a/g')
-            .then('sed s/o/e/g'))
-      .read())
+out = (cmd('echo moomoo')
+       .pipe(cmd('head -c 3').pipe('sed s/o/a/g')
+             .then('sed s/o/e/g'))
+       .read())
+print('nesting:', out)
 
 out = cd('/tmp').then('pwd').read()
 print('cd:', out)
