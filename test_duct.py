1beafdf399a94af098c16e48392876e76bc9aed9
Jack O'Connor <oconnor663@gmail.com>
2015-10-28 01:10:45 -0400

    start making tests Windows-compatible

--- test_duct.py
+++ test_duct.py
@@ -15,6 +15,47 @@ except ImportError:
     has_pathlib = False
 
 
+# Windows-compatible commands to mimic Unix
+# -----------------------------------------
+
+def exit_cmd(n, **kwargs):
+    return cmd('python', '-c', 'import sys; sys.exit({0})'.format(n), **kwargs)
+
+
+def true(**kwargs):
+    return exit_cmd(0, **kwargs)
+
+
+def false(**kwargs):
+    return exit_cmd(1, **kwargs)
+
+
+def head(c, **kwargs):
+    return cmd('python', '-c',
+               'import sys; sys.stdout.write(sys.stdin.read({0}))'.format(c),
+               **kwargs)
+
+
+def pwd(**kwargs):
+    return cmd('python', '-c', 'import os; print(os.getcwd())', **kwargs)
+
+
+def echo_x(**kwargs):
+    if os.name == 'nt':
+        return sh('echo %x%', **kwargs)
+    else:
+        return sh('echo $x', **kwargs)
+
+
+def replace(a, b, **kwargs):
+    return cmd(
+        'python', '-c', 'import sys; print(sys.stdin.read().replace({0}, {1}))'
+        .format(repr(a), repr(b)), **kwargs)
+
+
+# setup and teardown functions for the entire module
+# --------------------------------------------------
+
 def setup():
     '''Record the next available file descriptor. When each test finishes,
     check that the next available file descriptor is the same. That means we
@@ -30,6 +71,9 @@ def teardown():
     assert next_fd == new_fd, "We leaked a file descriptor!"
 
 
+# tests
+# -----
+
 def test_hello_world():
     out = sh('echo "hello  world"').read()
     assert "hello  world" == out
@@ -41,34 +85,34 @@ def test_result():
 
 
 def test_bytes():
-    out = sh('head -c 10 /dev/zero').read(stdout=bytes)
+    out = head(10).read(input="\x00"*100, stdout=bytes)
     assert b'\x00'*10 == out
 
 
 def test_nonzero_status_throws():
     with raises(duct.CheckedError):
-        cmd('false').run()
+        false().run()
 
 
 def test_check():
     # Test both the top level and command level check params.
-    assert 1 == cmd('false').run(check=False).status
-    assert 0 == cmd('false', check=False).run().status
+    assert 1 == false().run(check=False).status
+    assert 0 == false(check=False).run().status
 
 
 def test_pipe():
-    out = sh('head -c 3 /dev/zero').pipe('sed', 's/./a/g').read()
+    out = head(3).pipe(replace('x', 'a')).read(input="xxx")
     assert "aaa" == out
 
 
 def test_then():
-    assert 'hi' == cmd('true').then('echo', 'hi').read()
-    assert '' == cmd('false').then('echo', 'hi').read(check=False)
+    assert 'hi' == true().then('echo', 'hi').read()
+    assert '' == false().then('echo', 'hi').read(check=False)
 
 
 def test_nesting():
-    innermost = cmd('true').then('cat')
-    middle = cmd('true').then(innermost)
+    innermost = true().then('cat')
+    middle = true().then(innermost)
     out = sh('echo hi').pipe(middle).read()
     assert 'hi' == out
 
@@ -76,26 +120,29 @@ def test_nesting():
 def test_cwd():
     # Test cwd at both the top level and the command level, and that either can
     # be a pathlib Path.
-    assert '/tmp' == cmd('pwd').read(cwd='/tmp')
-    assert '/tmp' == cmd('pwd', cwd='/tmp').read(cwd='/something/else')
+    tmpdir = tempfile.mkdtemp()
+    another = tempfile.mkdtemp()
+    assert tmpdir == pwd().read(cwd=tmpdir)
+    assert tmpdir == pwd(cwd=tmpdir).read(cwd=another)
     if has_pathlib:
-        assert '/tmp' == cmd('pwd').read(cwd=Path('/tmp'))
-        assert ('/tmp' == cmd('pwd', cwd=Path('/tmp'))
+        assert tmpdir == pwd().read(cwd=Path(tmpdir))
+        assert (tmpdir == pwd(cwd=Path(tmpdir))
                 .read(cwd='/something/else'))
 
 
 def test_env():
     # Test env at both the top level and the command level, and that values can
     # be pathlib Paths.
-    assert "/" == sh("bash -c 'echo $x'").read(env={'x': '/'})
-    assert "/" == sh("bash -c 'echo $x'", env={'x': '/'}).read()
+    assert "foo" == echo_x().read(env={'x': 'foo'})
+    assert "foo" == echo_x(env={'x': 'foo'}).read()
     if has_pathlib:
-        assert "/" == sh("bash -c 'echo $x'").read(env={'x': Path('/')})
-        assert "/" == sh("bash -c 'echo $x'", env={'x': Path('/')}).read()
+        assert "foo" == echo_x().read(env={'x': Path('foo')})
+        assert "foo" == echo_x(env={'x': Path('foo')}).read()
 
 
 def test_full_env():
-    assert "" == sh("bash -c 'echo $x'", full_env={}).read(env={'x': 'X'})
+    out = "%x%" if os.name is 'nt' else ""
+    assert out == echo_x(full_env={}).read(env={'x': 'X'})
 
 
 def test_env_with_full_env_throws():
@@ -117,8 +164,8 @@ def test_undefined_keyword_throws():
 
 
 def test_input():
-    out = cmd('sha1sum').read(input="foo")
-    assert '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33  -' == out
+    out = replace('o', 'a').read(input="foo")
+    assert 'faa' == out
 
 
 def test_stdin():
@@ -163,12 +210,12 @@ def test_stdout():
     result = sh('echo hi', stdout=STDERR).run(stdout=str, stderr=str)
     assert '' == result.stdout
     assert 'hi\n' == result.stderr
-    # from stderr with STDOUT
+    # from stderr with STDOUT (yes, this works on Windows)
     result = sh('echo hi 1>&2', stderr=STDOUT).run(stdout=bytes, stderr=bytes)
     assert b'hi\n' == result.stdout
     assert b'' == result.stderr
     # full swap
-    result = (sh('echo hi; echo lo 1>&2', stdout=STDERR, stderr=STDOUT)
+    result = (sh('echo hi && echo lo 1>&2', stdout=STDERR, stderr=STDOUT)
               .run(stdout=str, stderr=str))
     assert 'lo\n' == result.stdout
     assert 'hi\n' == result.stderr
@@ -176,13 +223,19 @@ def test_stdout():
 
 def test_commands_can_be_paths():
     if has_pathlib:
-        echo = Path('/bin/echo')
-        assert 'foo' == cmd(echo, 'foo').read()
-        assert '\n' == sh(echo).read(trim=False)
+        tempdir = tempfile.mkdtemp()
+        path = Path(tempdir, "script.py")
+        with path.open('w') as f:
+            f.write('#! /usr/bin/env python\nprint("some stuff")')
+        path.chmod(0o755)
+        assert 'some stuff' == cmd(path).read()
+        assert 'some stuff\n' == sh(path).read(trim=False)
 
 
 def test_subshell():
-    c = sh("echo foo >&2 ; false")
+    # Note, don't put a space before the redirect, because Windows will keep
+    # that in the output.
+    c = sh("echo foo>&2").then(false())
     out = c.subshell(check=False, stderr=STDOUT).read()
     assert "foo" == out
 
@@ -194,15 +247,15 @@ def test_kwargs_prohibited_with_expression_value():
 
 
 def test_pipe_returns_rightmost_error():
-    assert 1 == sh('true').pipe('false').run(check=False).status
-    assert 1 == cmd('false').pipe('false').run(check=False).status
-    assert (3 == cmd('false').pipe(sh('bash -c "exit 3"')).run(check=False)
+    assert 1 == true().pipe(false()).run(check=False).status
+    assert 1 == false().pipe(false()).run(check=False).status
+    assert (3 == false().pipe(exit_cmd(3)).run(check=False)
             .status)
 
 
 def test_checked_error_contains_status():
     try:
-        sh('bash -c "exit 123"').run()
+        exit_cmd(123).run()
     except duct.CheckedError as e:
         assert '123' in str(e)
 
@@ -247,4 +300,4 @@ def test_write_error_in_input_thread():
     the pipe, and then that write will fail. Test that we catch this
     BrokenPipeError.'''
     test_input = '\x00' * 100 * 1000
-    sh('true').run(input=test_input)
+    true().run(input=test_input)
