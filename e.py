fd7db06f827d161546dea8ba31af9823d0585228
Jack O'Connor <oconnor663@gmail.com>
2015-10-30 23:34:06 -0400

    lots of printing

--- e.py
+++ e.py
@@ -4,18 +4,31 @@ from duct import cmd
 import textwrap
 
 
-def cat():
+def cat(arg):
     code = textwrap.dedent('''\
+        import os
         import sys
+        import random
+        import time
+        time.sleep(random.random())
+        print('starting.', '{0}', file=sys.stderr)
+        for i in range(10):
+            try:
+                print(i, os.stat(i) and True, file=sys.stderr)
+            except OSError:
+                pass
+        time.sleep(random.random())
+        print('reading.', '{0}', file=sys.stderr)
         input = sys.stdin.read()
+        time.sleep(random.random())
+        print('read finished. writing.', '{0}', file=sys.stderr)
         sys.stdout.write(input)
-        ''')
+        time.sleep(random.random())
+        print('write finished.', '{0}', file=sys.stderr)
+        '''.format(arg))
     return cmd('python', '-c', code)
 
 
-print("starting replace")
-out = cat().read(input='first')
-print("got", repr(out))
 print("starting pipe")
-out = cat().pipe(cat()).read(input="second")
+out = cat('left').pipe(cat('right')).read(input="second")
 print("got", repr(out))
