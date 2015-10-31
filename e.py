86e55dd4947b22e4316456047e29dbd66115c4aa
Jack O'Connor <oconnor663@gmail.com>
2015-10-30 21:10:30 -0400

    works with a writer thread too :(

--- e.py
+++ e.py
@@ -2,6 +2,7 @@
 
 import os
 from subprocess import Popen
+from threading import Thread
 
 cat = ['python', '-c', 'import sys; sys.stdout.write(sys.stdin.read())']
 
@@ -14,10 +15,19 @@ os.close(inputr)
 os.close(piper)
 os.close(pipew)
 os.close(outputw)
-print("writing")
-os.write(inputw, b"foo")
-os.close(inputw)
-print("wrote. waiting.")
-stdout = os.read(outputr, 100)
-os.close(outputr)
+
+
+def write():
+    with open(inputw, 'wb') as inputf:
+        inputf.write(b"foo")
+writer = Thread(target=write)
+print("spawning writer.")
+writer.start()
+
+print("reading.")
+with open(outputr, 'rb') as outputf:
+    stdout = outputf.read()
 print("got:", stdout)
+
+print("joining writer.")
+writer.join()
