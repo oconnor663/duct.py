56c5a0dc32ec36faddd48afc4a3b0008b9403c0e
Jack O'Connor <oconnor663@gmail.com>
2015-10-30 23:10:55 -0400

    minimal hanging example

--- e.py
+++ e.py
@@ -1,33 +1,21 @@
 #! /usr/bin/env python3
 
-import os
-from subprocess import Popen
-from threading import Thread
-
-cat = ['python', '-c', 'import sys; sys.stdout.write(sys.stdin.read())']
-
-inputr, inputw = os.pipe()
-piper, pipew = os.pipe()
-outputr, outputw = os.pipe()
-Popen(cat, stdin=inputr, stdout=pipew)
-Popen(cat, stdin=piper, stdout=outputw)
-os.close(inputr)
-os.close(piper)
-os.close(pipew)
-os.close(outputw)
+from duct import cmd
+import textwrap
 
 
-def write():
-    with open(inputw, 'wb') as inputf:
-        inputf.write(b"foo")
-writer = Thread(target=write)
-print("spawning writer.")
-writer.start()
+def cat():
+    code = textwrap.dedent('''\
+        import sys
+        input = sys.stdin.read()
+        sys.stdout.write(input)
+        ''')
+    return cmd('python', '-c', code)
 
-print("reading.")
-with open(outputr, 'rb') as outputf:
-    stdout = outputf.read()
-print("got:", stdout)
 
-print("joining writer.")
-writer.join()
+print("starting replace")
+out = cat().read(input='first')
+print("got", repr(out))
+print("starting pipe")
+out = cat().pipe(cat()).read(input="second")
+print("got", repr(out))
