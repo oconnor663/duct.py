21058ba0446feeed096721034448dc7376364158
Jack O'Connor <oconnor663@gmail.com>
2015-11-02 09:49:13 -0500

    simple example with Popen doesn't work

--- e.py
+++ e.py
@@ -1,34 +1,42 @@
 #! /usr/bin/env python3
 
-from duct import cmd
-import textwrap
+import subprocess
+import threading
+import os
+import time
+
+cat = '''
+import sys
+while True:
+    c = sys.stdin.read(1)
+    if not c:
+        break
+    sys.stdout.write(c)
+'''
+
+cat_cmd = ['python', '-c', cat]
+
+read_pipe, write_pipe = os.pipe()
 
 
-def cat(arg):
-    code = textwrap.dedent('''\
-        import os
-        import sys
-        import random
-        import time
-        time.sleep(random.random())
-        print('starting.', '{0}', file=sys.stderr)
-        for i in range(10):
-            try:
-                print(i, os.stat(i) and True, file=sys.stderr)
-            except OSError:
-                pass
-        time.sleep(random.random())
-        print('reading.', '{0}', file=sys.stderr)
-        input = sys.stdin.read()
-        time.sleep(random.random())
-        print('read finished. writing.', '{0}', file=sys.stderr)
-        sys.stdout.write(input)
-        time.sleep(random.random())
-        print('write finished.', '{0}', file=sys.stderr)
-        '''.format(arg))
-    return cmd('python', '-c', code)
+def left():
+    print("starting left cat...")
+    left_proc = subprocess.Popen(
+        cat_cmd, stdin=subprocess.PIPE, stdout=write_pipe)
+    time.sleep(0.5)
+    print("communicating with left cat...")
+    left_proc.communicate(input=b"foo")
+    print("finished left cat, closing left end of the pipe...")
+    os.close(write_pipe)
+    print("left end closed.")
+left_thread = threading.Thread(target=left)
+left_thread.start()
 
-
-print("starting pipe")
-out = cat('left').pipe(cat('right')).read(input="stuff")
-print("got", repr(out))
+print('starting right cat...')
+right_proc = subprocess.Popen(cat_cmd, stdin=read_pipe, stdout=subprocess.PIPE)
+time.sleep(0.5)
+print('communicating with right cat.')
+out = right_proc.communicate()
+print('finished right cat, closing right end of the pipe...')
+os.close(read_pipe)
+print('right end closed. got:', repr(out))
