04b0fc7e832c5e472bb58e9c8c245a387dbe3683
Jack O'Connor <oconnor663@gmail.com>
2016-06-02 01:41:58 -0400

    more setup.py fields

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='duct',
+    description='a library for shelling out and piping subprocesses'
+    license='MIT'
+    url='https://github.com/oconnor663/duct.py',
+    author="Jack O'Connor",
+    author_email='oconnor663@gmail.com'
     version='0.3.0',
     py_modules=['duct'],
 )
