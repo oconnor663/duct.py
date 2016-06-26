6d8a5a7bf6056fdad786d22b91813ea606b6e583
Jack O'Connor <oconnor663@gmail.com>
2016-06-25 18:05:53 -0700

    bump version to 0.4.0
    Changes since 0.3.0:
    - The magic constants NULL/CAPTURE/STDOUT/STDERR have been replaced with
      dedicated methods: null_*/capture_*/stdout_to_stderr/stderr_to_stdout.
      This saves you the trouble of extra imports, and it avoids weird
      typechecking issues like .stdin(CAPTURE) (if that's supposed to be a
      type error, then would need need two different types for NULL).
    - The env_remove and env_clear methods have been dropped, and we've gone
      back to the env/full_env API. (Though this time around they're methods
      instead of flags.) This is easier to use and more flexible, and it
      also prevents weird race conditions.
    - The cwd method has been renamed to dir. The cmd and cwd names were too
      awkwardly close to each other.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setuptools.setup(
     url='https://github.com/oconnor663/duct.py',
     author="Jack O'Connor",
     author_email='oconnor663@gmail.com',
-    version='0.3.0',
+    version='0.4.0',
     py_modules=['duct'],
 )
