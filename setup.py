555a695bd2b8fe715e28505a03a8e59f822ad0ce
Jack O'Connor <oconnor663@gmail.com>
2017-10-16 20:18:42 -0400

    bump version to 0.5.1
    Changes since 0.5.0:
    - Added `env_remove`.
    - Automatically uppercase env var names on Windows.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setuptools.setup(
     url='https://github.com/oconnor663/duct.py',
     author="Jack O'Connor",
     author_email='oconnor663@gmail.com',
-    version='0.5.0',
+    version='0.5.1',
     py_modules=['duct'],
 )
