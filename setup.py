28729dd0d9e61495c8c11a95346872741f38b483
Jack O'Connor <oconnor663@gmail.com>
2019-09-22 12:11:17 -0400

    long_description_content_type='text/markdown'
    This was preventing PyPI upload.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(
     name='duct',
     description='a library for running child processes',
     long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     license='MIT',
     url='https://github.com/oconnor663/duct.py',
     author="Jack O'Connor",
