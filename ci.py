498e04322f447112dc6b2ec96bb85e8c6f0bfc93
Jack O'Connor <oconnor663@gmail.com>
2019-10-09 13:11:16 -0400

    factor out ci.py

---
+++ ci.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import sys
+
+pytest_cmd = [
+    sys.executable,
+    "-m",
+    "pytest",
+    "duct.py",
+    "test_duct.py",
+    "--cov=duct",
+    "--cov-branch",
+]
+
+# Doctests are only compatible with Python 3 and non-Windows.
+if sys.version_info.major > 2 and os.name != "nt":
+    pytest_cmd.append("--doctest-modules")
+
+print("Executing:", " ".join(pytest_cmd))
+subprocess.check_call(pytest_cmd)
+
+print("Executing: flake8")
+subprocess.check_call(["flake8"])
+
+print("Success!")
