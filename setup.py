bf3e958fea6ce22a1832d9ca25ee69add60d720c
Jack O'Connor <oconnor663@gmail.com>
2025-05-06 17:44:58 -0700

    version 1.0.0
    Changes since 0.6.4:
    - The majority of the API is stabilized as-is. The main visible change
      is that the `try_wait` methods on `Handle` and `ReaderHandle` have
      been renamed to `poll`, for consistency with `subprocess.Popen.poll`
      in the Python standard library. The `try_wait` methods are retained as
      `@deprecated` aliases. Apart from that renaming, the rest of the
      changes are bugfixes or edge case behaviors.
    - Fixed a bug where `Handle.poll` could block indefinitely if a child
      process had exited but a longer-lived grandchild process was keeping
      its IO pipes open and its IO threads running (i.e. `stdin_bytes`,
      `stdout_capture`, or `stderr_capture`). Commit 60f57ae.
    - `Handle.kill` and `ReaderHandle.kill` no longer wait on child
      processes to exit. `subprocess.Popen` has an undocumented `__del__`
      finalizer, and it lazily cleans up leaked zombie children whenever new
      child processes are spawned. That's sufficient to prevent unbounded
      leaks, and there are also edge cases where waiting on a killed process
      can still block, so Duct now relies entirely on that finalizer. I'm
      porting similar behavior to the Rust implementation, for its
      coordinated v1.0.0 release.
    - Duct no longer kills child processes in any circumstance other than
      when you call `.kill()`. Previously Duct would kill child processes
      when closing a `ReaderHandle` that had not been read to EOF, or in
      some pipe spawning error cases.
    - The `ReaderHandle.close` method no longer has any effect. Duct no
      longer defines the method, and `ReaderHandle` inherits the no-op
      `IOBase.close`. `ReaderHandle` is still compatible with the `with`
      keyword, but context exit no longer has any effect. The `with` keyword
      has been removed from docs examples, to avoid giving the impression
      that it can do prompt cleanup without relying on finalizers.

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setuptools.setup(
     url="https://github.com/oconnor663/duct.py",
     author="Jack O'Connor",
     author_email="oconnor663@gmail.com",
-    version="0.6.4",
+    version="1.0.0",
     py_modules=["duct"],
 )
